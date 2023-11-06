42e55eb0839bf462e76a9bcbfe8f390307ea7ab8
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-06 23:42:11 +0530

    MyJJ6-2

---
+++ MySpring_Boot_a2/src/main/java/com/MySpringBootA2Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootA2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootA2Application.class, args);
+	}
+
+}
