4d0294e20de9d2c863e6883e420e431251d88cd3
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 08:37:36 +0530

    MyJJ6-19

---
+++ MySpring_Boot_aa10_Bidirectional_Relationships_JsonManagedReference_JsonBackReference/src/main/java/com/MySpringBootAa10BidirectionalRelationshipsJsonManagedReferenceJsonBackReferenceApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAa10BidirectionalRelationshipsJsonManagedReferenceJsonBackReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAa10BidirectionalRelationshipsJsonManagedReferenceJsonBackReferenceApplication.class, args);
+	}
+
+}
