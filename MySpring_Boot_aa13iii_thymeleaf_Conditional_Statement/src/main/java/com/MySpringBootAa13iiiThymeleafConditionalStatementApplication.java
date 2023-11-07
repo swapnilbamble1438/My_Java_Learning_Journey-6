e0f9c87e40566fbbe3b68a7e3278470837130d3f
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 08:47:00 +0530

    MyJJ6-25

---
+++ MySpring_Boot_aa13iii_thymeleaf_Conditional_Statement/src/main/java/com/MySpringBootAa13iiiThymeleafConditionalStatementApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAa13iiiThymeleafConditionalStatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAa13iiiThymeleafConditionalStatementApplication.class, args);
+	}
+
+}
