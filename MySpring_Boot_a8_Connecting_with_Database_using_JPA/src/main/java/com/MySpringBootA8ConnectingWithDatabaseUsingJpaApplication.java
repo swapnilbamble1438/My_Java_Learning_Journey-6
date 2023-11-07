f689de7d0512ec4f97932dfecb01b16623bdfa1d
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 08:35:34 +0530

    MyJJ6-17

---
+++ MySpring_Boot_a8_Connecting_with_Database_using_JPA/src/main/java/com/MySpringBootA8ConnectingWithDatabaseUsingJpaApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootA8ConnectingWithDatabaseUsingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootA8ConnectingWithDatabaseUsingJpaApplication.class, args);
+	}
+
+}
---
+++ MySpring_Boot_a8_Connecting_with_Database_using_JPA/src/main/java/com/model/Book.java
@@ -0,0 +1,58 @@
+package com.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Book")
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int id;
+	private String title;
+	private String author;
+	public Book() {
+		
+		super();
+		
+	}
+	public Book(int id, String title, String author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
+
+	
+	
+	
+	
+}
