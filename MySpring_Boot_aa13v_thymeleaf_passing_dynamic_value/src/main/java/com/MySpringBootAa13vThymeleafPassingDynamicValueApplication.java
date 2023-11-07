901ed33c39c3489fb5159dead024473c858e2580
swapnilbamble1438 <swapnilbamble1438@users.noreply.github.com>
2023-11-07 08:51:50 +0530

    MyJJ6-28

---
+++ MySpring_Boot_aa13v_thymeleaf_passing_dynamic_value/src/main/java/com/MySpringBootAa13vThymeleafPassingDynamicValueApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAa13vThymeleafPassingDynamicValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAa13vThymeleafPassingDynamicValueApplication.class, args);
+	}
+
+}
